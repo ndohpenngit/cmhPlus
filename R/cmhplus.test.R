8e9fd1e23cc8bd34b1a21062796141567abfe239
ndohpenngit <ndohpenn9@gmail.com>
2021-06-30 14:48:23 +0200

    ...

--- R/cmhplus.test.R
+++ R/cmhplus.test.R
@@ -1,8 +1,6 @@
 
-cmhplus.test <- function(X, Y, h, statistic, method)
+cmhplus.test <- function(X, Y, h, method)
 {
-  dname <- deparse(substitute(x))
-
   if(is.array(x)) {
     if(length(dim(x)) == 3L) {
       if(any(dim(x) < 2L)) stop("each dimension in table must be >= 2")
@@ -17,26 +15,33 @@ cmhplus.test <- function(X, Y, h, statistic, method)
 
     if(any(diff(c(length(X), length(Y), length(h))) != 0L))
       stop("'x', 'Y', and 'h' must have the same length")
-    dimname <- paste(dname, "and", deparse(substitute(y)), "and", deparse(substitute(h)))
    if((nlevels(X) < 2L) || (nlevels(Y) < 2L))
       stop("'X' and 'Y' must have at least 2 levels")
     else
      x <- table(X, Y, h)
   }
 
-  ## General CMH form
-
-  S_h <-
-  var_h <-
-  statistic <- sum(S_h)/sum(sqrt(var_h))
-  method <- c("chi-squared test", "correlation",)
+  method <- c("chi-squared test", "correlation",...)
 
   ## Pearson's chi-squared test
   if (method == "Pearson"){
-    chisq_test= #call chi-squared test
     if(!is.character(y))
       stop("'y' must be a character")
+    STATISTIC <- #chi-squared statistic
+    PARAMETER <- 1
+    PVAL <- pchisq(STATISTIC, PARAMETER, lower.tail = FALSE)# if (alternative == "two.sided")
   }
+
+  names(STATISTIC) <- "Mantel-Haenszel X-squared"
+  names(PARAMETER) <- "df"
+  METHOD <- paste("Mantel-Haenszel chi-squared test")
+
+  ## General CMH form
+  S_h <-
+  var_h <-
+  CMH.ESTIMATE <- sum(S_h)/sum(sqrt(var_h))
+
+
   ## Correlation test
   if (method == "correlation"){
     corr_test= #call correlation test
